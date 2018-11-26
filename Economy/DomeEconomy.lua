5a3b8969d0b9d4cb53bcb2429d0e727c7790bb63
GamingWubba93 <GamingWubba93@users.noreply.github.com>
2018-11-26 00:38:32 -0500

    Add files via upload

---
+++ Economy/DomeEconomy.lua
@@ -0,0 +1,21 @@
+function Dome:GetAverageWage()
+  local wage_sum, wage_earners, average_wage = 0, 0, 0
+  local colonists = self.labels.Colonist
+  for i = #colonists, 1, -1 do
+    local colonist = colonists[i]
+    
+    if colonist.workplace then
+      -- Fail-safe
+      if colonist.workplace.wages_level == nil then colonist.workplace.wages_level = 0 end
+      --
+      wage_sum = wage_sum + colonist.workplace.wages_level
+      wage_earners = wage_earners + 1
+    end
+    
+    if wage_earners > 0 then
+      average_wage = floatfloor((wage_sum + 0.0) // (wage_earners + 0.0))
+    end
+  end
+  
+  return average_wage
+end
---
+++ Economy/ServiceEconomy.lua
@@ -0,0 +1,22 @@
+function CanServiceEmployed(service, unit)
+  local is_child = unit.traits.Child
+  if is_child and not service.usable_by_children or not is_child and service.children_only then
+    return false
+  end
+  if service:DoesHaveConsumption() and service.consumption_type == g_ConsumptionType.Visit and service.consumption_stored_resources <= 0 then
+    return false
+  end
+  
+  -- Fail-safe
+  if service.prices_level == nil or service.prices_level == true then service.prices_level = 0 end
+  if unit.workplace and unit.workplace.wages_level == nil then unit.workplace.wages_level = 0 end
+  --
+  
+  if (unit.workplace and unit.workplace.wages_level >= service.prices_level or service.prices_level == 0) then
+    return true
+  end
+  
+  return false
+end
+
+Service.CanService = CanServiceEmployed
