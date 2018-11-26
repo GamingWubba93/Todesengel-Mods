f874a5149031786e4554d8cb417b6e0a8b2a6031
GamingWubba93 <GamingWubba93@users.noreply.github.com>
2018-11-26 17:03:12 -0500

    Add files via upload

--- ISHC2/Data/metadata.lua
+++ ISHC2/Data/metadata.lua
@@ -1,13 +1,15 @@
+-- ========== THIS IS AN AUTOMATICALLY GENERATED FILE! ==========
+
 return PlaceObj('ModDef', {
 	'title', "ISHC2 (2.3.x)",
 	'image', "Images/ISHC2 Preview Image.png",
 	'id', "tJ4TtfS",
 	'steam_id', "1332103888",
 	'author', "Obersturmbannführer Todesengel",
-	'version', 501,
+	'version', 502,
 	'lua_revision', 237920,
 	'code', {"Code/DroneRangeIncrease.lua","Code/ISHC2_1Init.lua","Code/IncreasedDomeWorkArea.lua"},
-	'saved', 1543269126,
+	'saved', 1543269311,
 	'TagGameplay', true,
 	'TagBuildings', true,
 	'TagResearch', true,
