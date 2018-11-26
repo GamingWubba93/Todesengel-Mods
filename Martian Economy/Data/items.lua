bad9ec8a0b5e3095323625732e64e33eb4e7c67e
GamingWubba93 <GamingWubba93@users.noreply.github.com>
2018-11-26 04:14:04 -0500

    Add files via upload

--- Martian Economy/Data/items.lua
+++ Martian Economy/Data/items.lua
@@ -1,1 +1,30 @@
-
+return {
+PlaceObj('ModItemCode', {
+	'name', "ColonistEconomy",
+	'FileName', "Code/ColonistEconomy.lua",
+}),
+PlaceObj('ModItemCode', {
+	'name', "DomeEconomy",
+	'FileName', "Code/DomeEconomy.lua",
+}),
+PlaceObj('ModItemCode', {
+	'name', "EconomyUI",
+	'FileName', "Code/EconomyUI.lua",
+}),
+PlaceObj('ModItemCode', {
+	'name', "ResidenceEconomy",
+	'FileName', "Code/ResidenceEconomy.lua",
+}),
+PlaceObj('ModItemCode', {
+	'name', "MarEcon_1Init",
+	'FileName', "Code/MarEcon_1Init.lua",
+}),
+PlaceObj('ModItemCode', {
+	'name', "ServiceEconomy",
+	'FileName', "Code/ServiceEconomy.lua",
+}),
+PlaceObj('ModItemCode', {
+	'name', "WorkplaceEconomy",
+	'FileName', "Code/WorkplaceEconomy.lua",
+}),
+}
---
+++ Martian Economy/Data/metadata.lua
@@ -0,0 +1,18 @@
+return PlaceObj('ModDef', {
+	'title', "Martian Economy",
+	'image', "Images/kisspng-economy-economics-economic-growth-computer-icons-e-economic-5abf23f51eba88.9945682615224760211259.png",
+	'last_changes', "v.0.1 Beta Test Released",
+	'id', "skGjvVT",
+	'steam_id', "1575009362",
+	'author', "Obersturmbannführer Todesengel",
+	'version', 7,
+	'lua_revision', 237920,
+	'code', {"Code/ColonistEconomy.lua","Code/DomeEconomy.lua","Code/EconomyUI.lua","Code/ResidenceEconomy.lua","Code/MarEcon_1Init.lua","Code/ServiceEconomy.lua","Code/WorkplaceEconomy.lua"},
+	'saved', 1543222321,
+	'screenshot1', "Images/preview_wealth_system.png",
+	'screenshot2', "Images/preview_martian_economy_morale.png",
+	'screenshot3', "Images/preview_martian_economy_comfort.png",
+	'screenshot4', "Images/preview_nwadibuke.png",
+	'TagGameplay', true,
+	'TagInterface', true,
+})
